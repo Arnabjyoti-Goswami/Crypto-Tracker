82e5fe3b6b30e998be311da6ec6c6b52894e5bfe
Arnab <arnangoswamiam@gmail.com>
2023-10-23 22:42:59 +0530

    create initial routes using react router, logo and bg

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Logo from '../components/Logo.jsx';
+
+const Home = () => {
+  return (
+    <main className='w-full h-full flex flex-col content-center items-center relative text-white font-nunito'>
+      <div className='w-screen h-screen bg-gray-300 fixed -z-10' />
+      <Logo />
+      <Outlet />
+    </main>
+  )
+}
+
+export default Home
