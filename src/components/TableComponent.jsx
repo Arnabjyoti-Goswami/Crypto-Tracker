da8d80dac055642bca36b583bc9f967659cf04d9
Arnab <arnangoswamiam@gmail.com>
2023-11-03 15:54:06 +0530

    setup crypto provider and star icon components
    use crypto context to get the data from coin gecko's free API that doesn't require an API key inside the table component, only render the table component if the data is not undefined

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,12 +1,14 @@
-import { CryptoContext } from "../context/CryptoContext.jsx"
+import { CryptoContext } from '../context/CryptoContext.jsx';
 import { useContext } from 'react';
+import StarSvg from '../assets/StarSvg.jsx';
 
 const TableComponent = () => {
-  const {  } = useContext(CryptoContext)
+  const { cryptoData } = useContext(CryptoContext);
 
   return (
     <div className='flex flex-col mt-9 border border-gray-100 rounded'>
-
+      {
+      cryptoData ? 
       <table className='w-full table-auto'>
         <thead className='capitalize text-base text-gray-100 font-medium border-b border-gray-100'>
           <tr>
@@ -21,9 +23,16 @@ const TableComponent = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className='text-center text-base border-b border-gray-100 hover:bg-gray-200 last:border-b-0'>
-            <td className='py-4'>asset</td>
-            <td className='py-4'>name</td>
+        {  
+        cryptoData.map(data => {
+          return(
+          <tr key={data.id} className='text-center text-base border-b border-gray-100 hover:bg-gray-200 last:border-b-0'>
+            <td className='py-4 flex items-center uppercase'>
+              <button className='outline-0 border-0 bg-none cursor-pointer'>
+                <StarSvg />
+              </button>
+            </td>
+            <td className='py-4'>{data.name}</td>
             <td className='py-4'>price</td>
             <td className='py-4'>total volume</td>
             <td className='py-4'>market cap change</td>
@@ -31,8 +40,13 @@ const TableComponent = () => {
             <td className='py-4'>24H</td>
             <td className='py-4'>7D</td>
           </tr>
+          )
+        })
+        }
         </tbody>
-      </table>
+      </table> 
+      : null
+      }
     </div>
   )
 }
