fecc3d2c45c5840e716c8807a7cfdc4ddf494baf
Arnab <arnangoswamiam@gmail.com>
2023-11-04 20:28:48 +0530

    setup react portal for popup details with charts for each coin

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,6 +1,16 @@
+import ReactDOM from 'react-dom';
+
 const CryptoDetails = () => {
-  return (
-    <div>CryptoDetails</div>
+  return ReactDOM.createPortal(
+    <div className='fixed top-0 w-full h-full
+    bg-gray-200 bg-opacity-30 backdrop-blur-sm
+    flex items-center justify-center font-nunito'>
+      <div className='w-[65%] h-[75%] relative rounded-lg
+      bg-gray-300 bg-opacity-75 text-white'>
+        CryptoDetails
+      </div>
+    </div> ,
+    document.getElementById("model")
   )
 }
 
