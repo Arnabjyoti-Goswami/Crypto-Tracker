1b606ef5e6becfb4dde5ebc2744b4e3f033816a8
Arnab <arnangoswamiam@gmail.com>
2023-11-04 21:14:55 +0530

    close coin data pop-up functionality using useNavigate

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,11 +1,16 @@
 import { useContext, useLayoutEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { CryptoContext } from '../context/CryptoContext.jsx';
 
 const CryptoDetails = () => {
   let { coinId } = useParams();
   let { getCoinData, coinData } = useContext(CryptoContext);
+  let navigate = useNavigate();
+
+  const closePopUp = () => {
+    navigate('..');
+  }
 
   useLayoutEffect(() => {
     getCoinData(coinId);
@@ -14,9 +19,13 @@ const CryptoDetails = () => {
   return ReactDOM.createPortal(
     <div className='fixed top-0 w-full h-full
     bg-gray-200 bg-opacity-30 backdrop-blur-sm
-    flex items-center justify-center font-nunito'>
+    flex items-center justify-center font-nunito'
+    onClick={closePopUp}>
       <div className='w-[65%] h-[75%] relative rounded-lg
-      bg-gray-300 bg-opacity-75 text-white'>
+      bg-gray-300 bg-opacity-75 text-white'
+      onClick={ (e) => {
+        e.stopPropagation()
+      } }>
       {
         coinData
         ? (
