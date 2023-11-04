a5157b894bcafdbe2f4acbf8bf898858f51cb28e
Arnab <arnangoswamiam@gmail.com>
2023-11-04 12:59:12 +0530

    pagination component complete

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,43 @@
+import { useState } from 'react';
 import paginationArrow from '../assets/pagination-arrow.svg';
 
 const Pagination = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  let totalPages = 100;
+
+  const nextPage = () => {
+    if(currentPage === totalPages) {
+      return null;
+    } else {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  const prevPage = () => {
+    if(currentPage === 1) {
+      return null;
+    } else {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  const multiStepNext = () => {
+    if(currentPage + 3 >= totalPages) {
+      setCurrentPage(totalPages - 1);
+    } else {
+      setCurrentPage(currentPage + 3);
+    }
+  }
+
+  const multiStepPrev = () => {
+    if(currentPage - 3 <= 1) {
+      setCurrentPage(currentPage + 1);
+    } else {
+      setCurrentPage(currentPage - 2);
+    }
+  }
+
   return (
     <div className='flex items-center justify-between mt-4 capitalize h-[2rem]'>
       <span>
@@ -11,52 +48,102 @@ const Pagination = () => {
       </span>
       <div className='flex items-center'>
         <ul className='flex items-center justify-end text-sm'>
+          {
+          currentPage !== 1 
+          ? (
+          <>
           <li className='flex items-center'>
             <button className='outline-0
-            hover:text-cyan w-8'>
-              <img className='w-full h-auto rotate-180' src={paginationArrow} alt='right'/>
-            </button>
-          </li>
-          <li>
-            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg'>
-              ...
+            hover:text-cyan w-8'
+            onClick={prevPage}>
+              <img className='w-full h-auto rotate-180' src={paginationArrow} alt='left'/>
             </button>
           </li>
           <li>
             <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center
-            bg-gray-200 mx-1.5'>
+            bg-gray-200 mx-1.5'
+            onClick={ () => {
+              setCurrentPage(1);
+            } }>
               1
             </button>
           </li>
+          </>
+          ) :
+          null
+          }
+          {
+          currentPage !== 1 && currentPage - 1 !== 1
+          ? (
+          <>
           <li>
-            <button className='outline-0 rounded-full w-8 h-8 flex items-center justify-center
-            bg-cyan text-gray-300 mx-1.5'>
-              2
-            </button>
-          </li>
-          <li>
-            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center
-            bg-gray-200 mx-1.5'>
-              3
-            </button>
-          </li>
-          <li>
-            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg'>
+            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg'
+            onClick={multiStepPrev}>
               ...
             </button>
           </li>
           <li>
             <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center
-            bg-gray-200 mx-1.5'>
-              100
+            bg-gray-200 mx-1.5'
+            onClick={prevPage}>
+              {currentPage - 1}
+            </button>
+          </li>
+          </>
+          ) :
+          null
+          }
+          <li>
+            <button className='outline-0 rounded-full w-8 h-8 flex items-center justify-center
+            bg-cyan text-gray-300 mx-1.5'>
+              {currentPage}
+            </button>
+          </li>
+          {
+          currentPage + 1 !== totalPages && currentPage !== totalPages
+          ? (
+          <>
+          <li>
+            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center
+            bg-gray-200 mx-1.5'
+            onClick={nextPage}>
+              {currentPage + 1}
+            </button>
+          </li>
+          <li>
+            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center text-lg'
+            onClick={multiStepNext}>
+              ...
+            </button>
+          </li>
+          </>
+          ) :
+          null
+          }
+          {
+          currentPage !== totalPages
+          ? (
+          <>
+          <li>
+            <button className='outline-0 hover:text-cyan rounded-full w-8 h-8 flex items-center justify-center
+            bg-gray-200 mx-1.5'
+            onClick={ () => {
+              setCurrentPage(totalPages);
+            } }>
+              {totalPages}
             </button>
           </li>
           <li className='flex items-center'>
             <button className='outline-0
-            hover:text-cyan w-8'>
-              <img className='w-full h-auto' src={paginationArrow} alt='left'/>
+            hover:text-cyan w-8'
+            onClick={nextPage}>
+              <img className='w-full h-auto' src={paginationArrow} alt='right'/>
             </button>
           </li>
+          </>
+          ) :
+          null
+          }
         </ul>
       </div>
     </div>
