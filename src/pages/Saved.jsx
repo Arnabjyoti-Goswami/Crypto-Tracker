22ed97b6c5e49649ea6132771c557c1e5bd1b61d
Arnab <arnangoswamiam@gmail.com>
2023-11-12 04:26:08 +0530

    display a msg in saved page when there is no saved data

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -9,7 +9,16 @@ const Saved = () => {
 
   return (
     <section className='w-[80%] h-full flex flex-col mt-4 mb-24 relative'>
+    {
+    savedCoinsData ? (
       <TableComponent data={savedCoinsData} currency={currency} />
+    ) : 
+    <div className='w-full mt-4 min-h-[60vh] py-8 border border-gray-100 rounded'>
+      <h1 className='min-h-[60vh] text-lg text-cyan flex items-center justify-center'>
+        There is no data to display!
+      </h1>
+    </div>
+    }
     <Outlet />
     </section>
   )
